839fea1103f565f7546f2ace2cdcf9972f2e3c6e
Alan Wills <alawills@googlemail.com>
2019-03-11 21:09:31 +0000

    Finished off hooking up UI and backend

--- SudokuSolver/Editors/SudokuSolver/ViewModels/SudokuSolverViewModel.cs
+++ SudokuSolver/Editors/SudokuSolver/ViewModels/SudokuSolverViewModel.cs
@@ -22,11 +22,37 @@ namespace SudokuSolver.Editors.SudokuSolver.ViewModels
             }
         }
 
+        // Top
+        public SudokuSubGridViewModel TopLeft { get; }
+        public SudokuSubGridViewModel TopMiddle { get; }
+        public SudokuSubGridViewModel TopRight { get; }
+
+        // Middle
+        public SudokuSubGridViewModel MiddleLeft { get; }
+        public SudokuSubGridViewModel MiddleMiddle { get; }
+        public SudokuSubGridViewModel MiddleRight { get; }
+
+        // Bottom
+        public SudokuSubGridViewModel BottomLeft { get; }
+        public SudokuSubGridViewModel BottomMiddle { get; }
+        public SudokuSubGridViewModel BottomRight { get; }
+
         #endregion
 
         public SudokuSolverViewModel(Sudoku sudoku) : 
             base(sudoku)
         {
+            TopLeft = new SudokuSubGridViewModel(sudoku.TopLeft);
+            TopMiddle = new SudokuSubGridViewModel(sudoku.TopMiddle);
+            TopRight = new SudokuSubGridViewModel(sudoku.TopRight);
+
+            MiddleLeft = new SudokuSubGridViewModel(sudoku.MiddleLeft);
+            MiddleMiddle = new SudokuSubGridViewModel(sudoku.MiddleMiddle);
+            MiddleRight = new SudokuSubGridViewModel(sudoku.MiddleRight);
+
+            BottomLeft = new SudokuSubGridViewModel(sudoku.BottomLeft);
+            BottomMiddle = new SudokuSubGridViewModel(sudoku.BottomMiddle);
+            BottomRight = new SudokuSubGridViewModel(sudoku.BottomRight);
         }
     }
 }
--- SudokuSolver/Editors/SudokuSolver/ViewModels/SudokuSubGridViewModel.cs
+++ SudokuSolver/Editors/SudokuSolver/ViewModels/SudokuSubGridViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SudokuSolver.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,42 @@ namespace SudokuSolver.Editors.SudokuSolver.ViewModels
 {
     public class SudokuSubGridViewModel
     {
+        #region Properties and Fields
+
+        public SudokuSubGrid SudokuSubGrid { get; }
+
+        // Top
+        public SudokuElementViewModel TopLeft { get; }
+        public SudokuElementViewModel TopMiddle { get; }
+        public SudokuElementViewModel TopRight { get; }
+
+        // Middle
+        public SudokuElementViewModel MiddleLeft { get; }
+        public SudokuElementViewModel MiddleMiddle { get; }
+        public SudokuElementViewModel MiddleRight { get; }
+
+        // Bottom
+        public SudokuElementViewModel BottomLeft { get; }
+        public SudokuElementViewModel BottomMiddle { get; }
+        public SudokuElementViewModel BottomRight { get; }
+
+        #endregion
+
+        public SudokuSubGridViewModel(SudokuSubGrid sudokuSubGrid)
+        {
+            SudokuSubGrid = sudokuSubGrid;
+
+            TopLeft = new SudokuElementViewModel(sudokuSubGrid.TopLeft);
+            TopMiddle = new SudokuElementViewModel(sudokuSubGrid.TopMiddle);
+            TopRight = new SudokuElementViewModel(sudokuSubGrid.TopRight);
+
+            MiddleLeft = new SudokuElementViewModel(sudokuSubGrid.MiddleLeft);
+            MiddleMiddle = new SudokuElementViewModel(sudokuSubGrid.MiddleMiddle);
+            MiddleRight = new SudokuElementViewModel(sudokuSubGrid.MiddleRight);
+
+            BottomLeft = new SudokuElementViewModel(sudokuSubGrid.BottomLeft);
+            BottomMiddle = new SudokuElementViewModel(sudokuSubGrid.BottomMiddle);
+            BottomRight = new SudokuElementViewModel(sudokuSubGrid.BottomRight);
+        }
     }
 }
