208c0aea0fdeee6372508ca3b22ad34a4ef7bf41
Alan Wills <alawills@googlemail.com>
2019-03-11 00:06:59 +0000

    Adding sudoku solver editor

---
+++ SudokuSolver/Editors/SudokuSolver/SudokuSolverView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using CelesteEngineEditor.Attributes;
+using CelesteEngineEditor.Editors;
+using DevZest.Windows.Docking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SudokuSolver.Editors.SudokuSolver
+{
+    /// <summary>
+    /// Interaction logic for SudokuSolver.xaml
+    /// </summary>
+    [CustomEditor("Sudoku Solver", DockPosition.Document)]
+    [CustomMenuItem("Solvers/Sudoku Solver")]
+    public partial class SudokuSolverView : Editor
+    {
+        public SudokuSolverView(object targetValue) :
+            base(new SudokuSolverViewModel(targetValue))
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SudokuSolver/Editors/SudokuSolver/SudokuSolverViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CelesteEngineEditor.Editors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Editors.SudokuSolver
+{
+    public class SudokuSolverViewModel : EditorViewModel
+    {
+        public SudokuSolverViewModel(object targetObject) : 
+            base(targetObject)
+        {
+        }
+    }
+}
